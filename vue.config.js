df4a9df8fca9d5667834532f54219331e1f80ac3
1DanToken <187533647+1DanToken@users.noreply.github.com>
2024-11-12 19:57:30 +0100

    Version update

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ const webpack = require('webpack');
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/1DanGame/' : '/',
   filenameHashing: true,
+  chainWebpack: (config) => {
+    config.output.filename('[name].[hash].js');
+    config.output.chunkFilename('[name].[hash].js');
+  },
   configureWebpack: {
     devtool: 'source-map',
     plugins: [
